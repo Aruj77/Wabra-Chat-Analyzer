8243318fc48374e53d3b945ae49c39e59eaf2a0c
Aruj Bansal <bansalaruj77@gmail.com>
2023-01-07 18:12:11 +0530

    Added Links counter

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+from urlextract import URLExtract
+ext=URLExtract()
 def fetch_stats(selected_user,df):
     if selected_user!='Overall':
         df=df[df['user']==selected_user.replace('\n',' ')]
@@ -9,6 +11,9 @@ def fetch_stats(selected_user,df):
         words.extend(i.split())
     #number of media
     num_media=df[df['message']=='<Media omitted>\n'].shape[0]
-    
-    
-    return num_messages,len(words),num_media
+    #number of links
+    links=[]
+    for i in df['message']:
+        links.extend(ext.find_urls(i))
+        
+    return num_messages,len(words),num_media,len(links)
